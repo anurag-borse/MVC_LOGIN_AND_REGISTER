925ad8df0281cffe219def42b279612bb40d9dd0
Anurag <jai.borse01@gmail.com>
2024-06-10 15:01:37 +0530

    Anurag

--- MVC_LOGIN_AND_REGISTER/Controllers/LoginController.cs
+++ MVC_LOGIN_AND_REGISTER/Controllers/LoginController.cs
@@ -40,8 +40,6 @@ namespace MVC_LOGIN_AND_REGISTER.Controllers
             {
                 return RedirectToAction("Login");
             }
-
-
         }
 
         [HttpGet]
--- MVC_LOGIN_AND_REGISTER/Models/User.cs
+++ MVC_LOGIN_AND_REGISTER/Models/User.cs
@@ -14,5 +14,7 @@ namespace MVC_LOGIN_AND_REGISTER.Models
 
         public string Username { get; set; }
         public string Password { get; set; }
+
+
     }
 }
